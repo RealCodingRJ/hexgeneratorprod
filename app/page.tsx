26356b4516195d281e09024e593adf3ae8333fef
RYAN JAMES <realcoder2023@gmail.com>
2025-11-30 16:34:26 -0500

    iniial release

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Fragment, useState } from "react";
+
+export default function Page() {
+  const [hexColor, setHexColor] = useState("#202020");
+
+  function getColorInput(e: any) {
+    setHexColor(e.target.value);
+  }
+
+  function setBackground(e: any) {
+    if (hexColor == "#") {
+      document.body.style.backgroundColor = "#" + hexColor;
+    } else {
+      document.body.style.backgroundColor = hexColor;
+    }
+  }
+
+  function Button() {
+    return (
+      <div>
+        <button onClick={setBackground}>GENERATE</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <header>
+        <div>
+          <a href=""></a>
+        </div>
+      </header>
+      <div className="container">
+        <main>
+          <div>
+            <h2>WELCOME TO HEX GENERATION APPLICATION</h2>
+
+            <input placeholder="Enter Hex Code: " onChange={getColorInput} />
+          </div>
+          <Button />
+        </main>
+      </div>
+    </div>
+  );
+}
